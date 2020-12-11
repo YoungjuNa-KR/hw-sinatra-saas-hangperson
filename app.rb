ccc506581d65e880173dae66cff7049d69574d5c
Youngju Na <apercent1@naver.com>
2020-12-11 02:15:07 +0000

    change all

--- app.rb
+++ app.rb
@@ -28,6 +28,9 @@ class HangpersonApp < Sinatra::Base
   post '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || HangpersonGame.get_random_word
+    # word = "ruby"
+  
+    
     # NOTE: don't change previous line - it's needed by autograder!
 
     @game = HangpersonGame.new(word)
@@ -40,6 +43,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    # 중복일 때 
+    begin
+      if @game.guess(letter) == false
+        flash[:message] = "You have already used that letter."
+      end
+    # Invalid한 경우
+    rescue ArgumentError
+      flash[:message] = "Invalid guess"
+    end
     redirect '/show'
   end
   
@@ -50,17 +62,24 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :win
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :lose
     erb :lose # You may change/remove this line
   end
-  
 end
